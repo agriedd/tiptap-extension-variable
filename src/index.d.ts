281890b5ae14b7c7353231016d431e0328bc41c2
Agri Edd. Oematan <dnr739528@gmail.com>
2025-07-26 07:42:54 +0800

    publish: update version

---
+++ src/index.d.ts
@@ -0,0 +1,3 @@
+export type { Variable, VariableNodeAttrs, VariableOptions } from './lib/variable'
+export type { Suggestion, SuggestionOptions, SuggestionKeyDownProps, SuggestionPluginKey } from './lib/suggestion/suggestion'
+export type { PopoverOptions, Popover, PopoverProps, PopoverKeyDownProps } from './lib/suggestion/popover'

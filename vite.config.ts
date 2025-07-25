3511f8d07677e33024dcb0bb961b219ca4d288dc
Agri Edd. Oematan <dnr739528@gmail.com>
2025-07-25 20:43:01 +0800

    fix: vite config bundle

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,25 @@
 import {defineConfig} from 'vite'
-import { resolve } from 'path';
-import react from '@vitejs/plugin-react'
+import {resolve} from 'path';
+import react from '@vitejs/plugin-react-oxc'
 // @ts-ignore unknown issue
 import tailwindcss from '@tailwindcss/vite'
 import packageJson from './package.json';
 import inspect from 'vite-plugin-inspect';
+import { visualizer } from "rollup-plugin-visualizer";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
-        react(), tailwindcss(), inspect()
+        react(), tailwindcss(), visualizer(),
+        inspect({
+            build: true,
+            outputDir: '.vite-inspect',
+        })
     ],
     build: {
         lib: {
             entry: resolve(__dirname, 'src/index.ts'),
             name: 'Variable',
-            formats: ['es'],
+            formats: ['es', 'umd'],
             fileName: (format) => `variable.${format}.js`,
         },
         sourcemap: true,
@@ -23,11 +28,26 @@ export default defineConfig({
             output: {
                 dir: 'dist',
                 globals: {
-                    '@tiptap/core': 'tiptapCore',
-                    '@tiptap/pm': 'tiptapPm',
+                    '@tiptap/core': '@tiptap/core',
+                    '@tiptap/pm': '@tiptap/pm',
+                    'prosemirror-model': 'prosemirror-model',
+                    'prosemirror-state': 'prosemirror-state',
+                    'prosemirror-view': 'prosemirror-view',
+                    'prosemirror-transform': 'prosemirror-transform',
                 },
             },
-            external: Object.keys(packageJson.peerDependencies || {}),
+            external: [
+                ...Object.keys(packageJson.peerDependencies || {}),
+                'prosemirror-model',
+                'prosemirror-state',
+                'prosemirror-view',
+                'prosemirror-transform',
+                '@tiptap/core',
+                '@tiptap/pm',
+                '@tiptap/pm/model',
+                '@tiptap/pm/state',
+                '@tiptap/pm/view',
+            ],
         },
         emptyOutDir: true,
         minify: 'oxc',
